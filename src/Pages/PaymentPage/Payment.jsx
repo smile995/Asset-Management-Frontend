b35be28948b28dc9cdee625c5ba17f3648f9a930
smile995 <ah.smile995@gmai.com>
2023-12-04 22:17:57 +0600

    no time to complete this assignment

--- src/Pages/PaymentPage/Payment.jsx
+++ src/Pages/PaymentPage/Payment.jsx
@@ -1,12 +1,18 @@
+import { Helmet } from "react-helmet";
 
-import { Helmet } from 'react-helmet';
 
 const Payment = () => {
+
     return (
         <div>
             <Helmet>
                 <title>Assets Management System | Payment</title>
             </Helmet>
+
+
+          <h1 className="text-center text-3xl font-bold ">Payment Page</h1>
+       
+
 
         </div>
     );
